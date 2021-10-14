4837232a70d86e66ee4387aaa8b37c722f0cc663
rani1029 <ranig1029@gmail.com>
2021-10-14 08:33:03 +0530

    Data inserted into Table  Created

--- EmployeePayroll2SQLQuery.sql
+++ EmployeePayroll2SQLQuery.sql
@@ -6,3 +6,6 @@ CREATE DATABASE payroll_service2;
 USE payroll_service;
 CREATE TABLE Employee_Payroll2(ID int identity(1,1) Primary Key,Name varchar(255),Salary float,StartDate Date);
 SELECT * FROM Employee_Payroll2;
+--uc3
+
+INSERT INTO Employee_Payroll2(Name,Salary,StartDate)VALUES('Rani','50000','2018-07-06');
