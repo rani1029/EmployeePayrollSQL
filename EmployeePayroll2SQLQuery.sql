690110294a27c007b37f2a42dec4d94cd9dd4c1a
rani1029 <ranig1029@gmail.com>
2021-10-14 08:40:21 +0530

    Data Retieved from Table  Created

--- EmployeePayroll2SQLQuery.sql
+++ EmployeePayroll2SQLQuery.sql
@@ -9,3 +9,6 @@ SELECT * FROM Employee_Payroll2;
 --uc3
 
 INSERT INTO Employee_Payroll2(Name,Salary,StartDate)VALUES('Rani','50000','2018-07-06');
+INSERT INTO Employee_Payroll2(Name,Salary,StartDate)VALUES('Nayan','30000','2017-07-05'),('Rohit','60000','2020-07-06');
+ --uc4
+SELECT * FROM Employee_Payroll2;
