5acedd4121a559e0d45a78c3e44d0b20f123f397
rani1029 <ranig1029@gmail.com>
2021-10-14 10:26:43 +0530

    use sql function using group by

--- EmployeePayroll2SQLQuery.sql
+++ EmployeePayroll2SQLQuery.sql
@@ -26,3 +26,35 @@ AS DATE) AND GETDATE();
 ALTER TABLE Employee_Payroll2 ADD Gender char(1); --it first set gender as null then it can be update as follows
 UPDATE Employee_Payroll2 SET Gender='M';
 UPDATE Employee_Payroll2 SET Gender='F' WHERE Name='Rani';
+
+--uc7
+--calculate sum of salary of specific gender
+SELECT SUM(Salary) FROM Employee_Payroll2
+WHERE Gender = 'F' GROUP BY Gender;
+SELECT SUM(Salary) FROM Employee_Payroll2
+WHERE Gender = 'M' GROUP BY Gender;
+
+--calculate Average of salary of specific gender
+
+SELECT AVG(Salary) FROM Employee_Payroll2
+WHERE Gender = 'M' GROUP BY Gender;
+
+
+SELECT AVG(Salary) FROM Employee_Payroll2
+WHERE Gender = 'F' GROUP BY Gender;
+
+--calculate MINIMUM of salary of specific gender
+
+SELECT MIN(Salary) FROM Employee_Payroll2
+WHERE Gender = 'M' GROUP BY Gender;
+
+SELECT MIN(Salary) FROM Employee_Payroll2
+WHERE Gender = 'F' GROUP BY Gender;
+
+--calculate MAXIMUM of salary of specific gender
+
+SELECT MAX(Salary) FROM Employee_Payroll2
+WHERE Gender = 'M' GROUP BY Gender;
+
+SELECT MAX(Salary) FROM Employee_Payroll2
+WHERE Gender = 'F' GROUP BY Gender;
