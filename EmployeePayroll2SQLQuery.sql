ea3066186c4fe5fdf232e494abeccc757b99f1ea
rani1029 <ranig1029@gmail.com>
2021-10-14 08:23:00 +0530

    Table  Created

--- EmployeePayroll2SQLQuery.sql
+++ EmployeePayroll2SQLQuery.sql
@@ -2,3 +2,7 @@
 --uc1
  
 CREATE DATABASE payroll_service2;
+--uc2
+USE payroll_service;
+CREATE TABLE Employee_Payroll2(ID int identity(1,1) Primary Key,Name varchar(255),Salary float,StartDate Date);
+SELECT * FROM Employee_Payroll2;
