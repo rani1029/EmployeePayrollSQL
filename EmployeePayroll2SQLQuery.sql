1d7ea1896cd2c42cbdfa15bfa33d0bc55b8f00e9
rani1029 <ranig1029@gmail.com>
2021-10-15 12:47:02 +0530

    Rename column name and add more column to table

--- EmployeePayroll2SQLQuery.sql
+++ EmployeePayroll2SQLQuery.sql
@@ -70,3 +70,27 @@ WHERE Gender = 'F' GROUP BY Gender;
 ALTER TABLE Employee_Payroll2 ADD Phone bigint,Address varchar(250),Department varchar(250) not null;
 UPDATE Employee_Payroll2 SET Address='INDIA';
 UPDATE Employee_Payroll2 SET Department='Finance';
+SELECT * FROM Employee_Payroll2;
+UPDATE Employee_Payroll2 set Phone='453267899',Address='chennai' where Name='Nayan';
+UPDATE Employee_Payroll2 set Phone='453267899',Address='Mumbai' where Name='Rani';
+UPDATE Employee_Payroll2 set Phone='453267899',Address='Banglore' where Name='Rohit';
+UPDATE Employee_Payroll2 set Department='HR'where Name='Nayan';
+UPDATE Employee_Payroll2 set Department='Sales'where Name='Rohit';
+
+UPDATE Employee_Payroll2 SET Phone='6754389654' where Name='Rani';
+UPDATE Employee_Payroll2 SET Phone='5454321154' where Name='Rohit';
+
+--Uc9
+---Rename Colomn Salary renamed as Basic pay---
+Exec sp_rename 'Employee_payroll2.Salary', 'BasicPay';
+alter table Employee_Payroll2 ADD  TaxablePay float, Deduction float,IncomeTax float,NetPay float;
+Update Employee_Payroll2 set Deduction = '3000' where Department = 'HR';
+Update Employee_Payroll2 set Deduction = '5000' where Department = 'Finance';
+Update Employee_Payroll2 set Deduction = '6000' where Department = 'Sales';
+Update Employee_Payroll2 set TaxablePay = null; 
+Update Employee_Payroll2 set IncomeTax = '2000'; 
+Update Employee_Payroll2 set NetPay = (BasicPay-Deduction);
+
+
+
+
